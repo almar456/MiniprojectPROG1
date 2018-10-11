8941fe3b3d1e4086527128de8261e909d5e47899
Almar <almar456@hotmail.com>
2018-10-11 17:43:46 +0200

    Stallen.py verbeterd, documentatie toegevoegd.

--- Stallen.py
+++ Stallen.py
@@ -14,15 +14,13 @@ def stallen(nummer: str):
             break
 
     try:                                                                    # Checken of fiets al gestald is, zo niet
-        if (lijst[index][4] == 'True'):                                     # dan de waarde 'gestald' naar True.
-            print('De fiets is al gestald.')                                # En de tijd toevoegen.
-            return 2
+        if (lijst[index][4] == 'True'):                                     # dan de waarde 'gestald' naar True
+            return 2                                                        # En de tijd toevoegen.
         elif lijst[index][4] == 'False':
             lijst[index][5] = datetime.datetime.today().strftime("%a %d %b %Y om %H:%M:%S")
             lijst[index][4] = 'True'
             print('De fiets is gestald.')
     except:
-        print('Incorrect nummer.')
         return 1
 
     newFile = ''                                                            # eventuele verandering opslaan in csv
