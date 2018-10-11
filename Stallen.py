cf09ace20097c09ac823124962f97d82c93e1702
Almar <almar456@hotmail.com>
2018-10-11 14:20:01 +0200

    Stalling programma verbeterd + tijd en csv file verbeterd

--- Stallen.py
+++ Stallen.py
@@ -1,4 +1,5 @@
-def stallen(login: str, wachtwoord: str):
+def stallen(nummer: str):
+    import datetime                                                         # Imports
 
     bestand = 'fietsen.csv'                                                 # Informatie ophalen en in lijst zetten
     file = open(bestand, 'r').read()
@@ -7,31 +8,31 @@ def stallen(login: str, wachtwoord: str):
     for x in lijst:
         lijst[lijst.index(x)] = x.split(';')
 
-    Correct = False                                                         # Check inloggegevens
-    for x in lijst:
-        if (login in x) and (wachtwoord in x):
+    for x in lijst:                                                         # Check nummer
+        if (str(nummer) == x[0]):
             index = lijst.index(x)
-            Correct = True
             break
 
     try:                                                                    # Checken of fiets al gestald is, zo niet
-        if (lijst[index][3] == 'True'):                                     # dan de waarde 'gestald' naar True
-            print('De fiets is al gestald.')
-        elif lijst[index][3] == 'False':
-            lijst[index][3] = 'True'
+        if (lijst[index][4] == 'True'):                                     # dan de waarde 'gestald' naar True.
+            print('De fiets is al gestald.')                                # En de tijd toevoegen.
+        elif lijst[index][4] == 'False':
+            lijst[index][4] = 'True'
             print('De fiets is gestald.')
-        print(lijst)
     except:
-        print('Incorrecte inloggegevens')
+        print('Incorrect nummer.')
+    lijst[index][5] = datetime.datetime.today().strftime("%a %d %b %Y om %H:%M:%S")
 
     newFile = ''                                                            # eventuele verandering opslaan in csv
     for x in lijst:
         if x == ['']:
             break
         for y in x:
-            if y != x[3]:
+            if y != x[5]:
                 newFile = newFile+y+';'
             else:
                 newFile = newFile+y+'\n'
-    print(newFile)
     open(bestand,'w').write(newFile)
+    open(bestand).close()
+
+stallen(1234567890)
