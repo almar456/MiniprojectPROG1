3c872b54e955e94c4c15ba62e0484ac500968d17
xSketchy0 <scoobyjai@hotmail.com>
2018-10-16 02:19:21 +0200

    Wachtwoord validatie toegevoegd.

--- Registreren.py
+++ Registreren.py
@@ -1,5 +1,6 @@
 def registreren(wachtwoord: str, username: str, naam: str):
     import csv
+    import re
     from random import randint
 
     # Genereer code d.m.v. randint
@@ -7,10 +8,31 @@ def registreren(wachtwoord: str, username: str, naam: str):
 
     # Gegevens die de gebruiker invoert in een list
     gegevens = [code, wachtwoord, username, naam, False, '-']
+    # Valideer het wachtwoord
 
-    # Schrijven naar de CSV file d.m.v. van import csv
-    with open('fietsen.csv', mode='a', newline="") as fietsen_file:
-        writer = csv.writer(fietsen_file, delimiter=';')
-        writer.writerow(gegevens)
+    valid = 0
 
-registreren('lalala', 'xSketfchy0', 'AJ')
+    while True:
+        if (len(wachtwoord) < 6):
+            flag = -1
+            break
+        elif not re.search("[a-z]", wachtwoord):
+            flag = -1
+            break
+        elif not re.search("[0-9]", wachtwoord):
+            flag = -1
+            break
+        else:
+            flag = 0
+            # Schrijven naar de CSV file d.m.v. van import csv
+            with open('fietsen.csv', mode='a', newline="") as fietsen_file:
+                writer = csv.writer(fietsen_file, delimiter=';')
+                writer.writerow(gegevens)
+                print("Succesfully registered.")
+                print("The code to stall your bike is: " + str(code))
+            break
+    if flag == -1:
+        return print("Password doesn't meet the requirements.")
+
+
+registreren('lalala6', 'AJSijpenhof1337', 'AJ')
